eb4764fdd27c9f416763217738f3cc1ab852f74f
Pedro Soares <ngrpedro22@gmail.com>
2022-12-04 18:22:49 -0300

    photo service + slice

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -1,5 +1,19 @@
 import { api, requestConfig } from "../utils/config";
 
-const photoService = {};
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(api + "/photos" + config)
+      .then((res) => res.jdon())
+      .catch((err) => err);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = {
+  publishPhoto,
+};
 
 export default photoService;
--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -10,6 +10,23 @@ const initialState = {
   message: null,
 };
 
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.publishPhoto(photo, token);
+
+    console.log(data.errors);
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -17,6 +34,26 @@ export const photoSlice = createSlice({
     resetMessage: (state) => {
       state.message = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
+        state.photos.unshift(state.photo);
+        state.message = "Foto publicada com sucesso!";
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      });
   },
 });
 
