fd94acabcd73cff58fc10eac14166ae86174bc7a
Pedro Soares <ngrpedro22@gmail.com>
2022-12-04 17:44:13 -0300

    form photo

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { getUserDetails } from "./../../slices/userSlice";
 import { uploads } from "../../utils/config";
+import { useRef } from "react";
 
 const Profile = () => {
   const { id } = useParams();
@@ -12,14 +13,22 @@ const Profile = () => {
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
 
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
+
   useEffect(() => {
     dispatch(getUserDetails(id));
   }, [dispatch, id]);
 
+  const submitHandle = (e) => {
+    e.preventDefault();
+    console.log("teste");
+  };
+
   return (
-    <div className="flex items-center justify-center p-10 h-[80vh]">
-      <div className="flex flex-col md:flex-row items-start justify-start md:gap-4">
-        <div className="w-36 h-36 rounded-md object-cover bg-gray-600">
+    <div className="flex flex-col items-start justify-center p-10">
+      <div className="flex flex-col md:flex-row items-start justify-start md:gap-10 w-full border-b border-gray-700 pb-10">
+        <div className="w-36 h-36 object-cover">
           {user.profileImage && (
             <img
               src={`${uploads}/users/${user.profileImage}`}
@@ -27,11 +36,38 @@ const Profile = () => {
             />
           )}
         </div>
-        <div className="flex flex-col gap-1 max-w-lg">
+        <div className="flex flex-col gap-1">
           <h1 className="font-bold text-2xl">{user.name}</h1>
           <h2 className="font-bold text-lg">{user.email}</h2>
-          <span>{user.bio}</span>
         </div>
+      </div>
+      <div className="py-20">
+        {id === userAuth._id && (
+          <>
+            <div className="space-y-5" ref={newPhotoForm}>
+              <h1>Compartilhe algum momento seu:</h1>
+              <form onSubmit={submitHandle}>
+                <label className="block">
+                  <span>Título da foto:</span>
+                  <input
+                    type="text"
+                    className="border border-gray-300 p-2 rounded-md w-full  text-gray-700"
+                  />
+                </label>
+
+                <label className="block">
+                  <span>Imagem:</span>
+                  <input
+                    type="file"
+                    className="border border-gray-300 p-2 rounded-md w-full  text-gray-700"
+                  />
+                </label>
+
+                <input type="submit" value="Postar" />
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
